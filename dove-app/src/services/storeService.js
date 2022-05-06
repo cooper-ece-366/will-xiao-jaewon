34f906badfbf0f37c18f1e66462c02a381752398
Xiao Lin <xiaolin1538@gmail.com>
2022-05-06 19:42:25 -0400

    Updated axios get methods for pagination, and added colored background to the density display

--- dove-app/src/services/storeService.js
+++ dove-app/src/services/storeService.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import {StoreApiUrl} from "../const";
+import {apiUrlPrefix, StoreApiUrl} from "../const";
 
 //Reference: https://codebun.com/search-record-from-a-table-in-react-js-spring-boot-and-mysql
 // Edited by Xiao Lin
 class storeService{
     getStore(){
         return axios.get(StoreApiUrl);
+    }
+    getStores =(currentPage, recordPerPage) =>{
+        return axios.get(apiUrlPrefix.concat("/store/?page="+currentPage+"&size="+recordPerPage))
+    }
+    storeSearch =(currentPage, recordPerPage, search) =>{
+        return axios.get(apiUrlPrefix.concat("/store/"+search+"?page="+currentPage+"&size="+recordPerPage))
     }
     create = data =>{
         return axios.post(StoreApiUrl,data)
